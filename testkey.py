4094d70c5f3a6eefbc3b47cd525dfb9319b0fb9f
pakitoalfa <56008649+pakitoalfa@users.noreply.github.com>
2020-07-31 12:12:52 -0500

    Update testkey.py
    ya no lanza tantos falsos negativos

--- testkey.py
+++ testkey.py
@@ -1,5 +1,6 @@
 import shodan
 import sys
+import time
 
 def test(key):
     api = shodan.Shodan(key)
@@ -7,17 +8,18 @@ def test(key):
     try:
         info = api.info()
     except Exception:
-        print "{-} Key %s es invalida!" %(key)
+        print "{-} Key %s es invalida  :-( " %(key)
         return False,False
     if info['plan'] == 'dev' or info['plan'] == 'edu': #this seems to be how they are categorized
-        print "{+} Key %s es valida y de pago :-)!!!" %(key)   
+        print "{+} Key %s es valida y de pago :-) !!! " %(key) + " ==>  " + info['plan']
 	return True,True
     elif info['plan'] == 'oss': # however I might be wrong. oh well.
-        print "{*} Key %s es valida pero gratuita!" %(key)
+        print "{*} Key %s es valida pero gratuita!" %(key) 
         return True,False
 
 
 def main(args):
+    print "===Shodankeys==="	
     if len(args) != 2:
         sys.exit("Shodan API Key List Checker (for testing githubbed keys)\nusage: %s keys-to-test.txt" %(args[0]))
     f = open(args[1], "r")
@@ -26,6 +28,7 @@ def main(args):
     paid_keys = []
     comm_keys = []
     for key in keys:
+	time.sleep(2);#el delay es para dar estabilidad
         key = key.strip()
         is_valid,is_paid = test(key=key)
         if is_valid == True:
@@ -36,13 +39,13 @@ def main(args):
                 comm_keys.append(key)
         else:
             pass
-    print "\n\n{+} Acquired %d valid keys" %(len(valid_keys))
-    print "{+} Acquired %d paid-keys" %(len(paid_keys))
-    print "{+} Acquired %d community-keys" %(len(comm_keys))
-    print "\n{+} Paid Keys..."
+    print "\n\n{+} tienes %d keys validas" %(len(valid_keys))
+    print "{+} tienes %d keys de pago" %(len(paid_keys))
+    print "{+} tienes %d keys gratuitas" %(len(comm_keys))
+    print "\n{+} keys de pago..."
     for key in paid_keys:
         print key
-    print "\n{+}Community Keys..."
+    print "\n{+}keys gratuitas..."
     for key in comm_keys:
         print key
 
